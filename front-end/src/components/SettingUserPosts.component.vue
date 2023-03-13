7819d11f1acf5aa156340c624197f640fbb8722b
杜羽 <90547641+xCk27x@users.noreply.github.com>
2023-03-13 01:46:43 +0000

    unfinished SettingPosts and ShortPagePost

--- front-end/src/components/SettingUserPosts.component.vue
+++ front-end/src/components/SettingUserPosts.component.vue
@@ -1,1 +1,39 @@
-<template></template>
+<template>
+  <div class="userposts">
+    <div class="userposts-header"></div>
+    <div class="userposts-body"></div>
+  </div>
+</template>
+
+<script>
+import ShortPost from './ShortPagePost.vue'
+import { ref } from 'vue';
+
+export default {
+  components: {
+    'short-post': ShortPost,
+  },
+  setup() {
+    const articles = ref([]);
+
+    const getNPostRandom = async (num) => {
+      try {
+        const response = await axios.get(
+          `http://localhost:3000/api/post/random/${num}`,
+          {
+            headers: {
+              Authorization: "Bearer " + localStorage.getItem("token"),
+            },
+          }
+        );
+        console.log(response);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    return {
+      articles,
+    };
+  }
+} 
+</script>
